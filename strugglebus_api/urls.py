06a48e430304dddc9381f6d1b5eab5f2b227757d
Brooke Nemchak <bnemchak@gmail.com>
2021-03-24 22:31:49 -0500

    task pulling

--- strugglebus_api/urls.py
+++ strugglebus_api/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include
 from django.urls import path
-from strugglebusapi.views import register_user, login_user
+from strugglebusapi.views import register_user, login_user, TasksViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'tasks', TasksViewSet, 'task')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),
 ]
