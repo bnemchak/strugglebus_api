004e19c48d1d3ee7fe44ff559d0a10c00c0757c0
Brooke Nemchak <bnemchak@gmail.com>
2021-03-25 14:30:57 -0500

    WIP

--- strugglebusapi/views/views.py
+++ strugglebusapi/views/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render
-from rest_framework import viewsets, serializers
+from django.core.exceptions import ValidationError
+from rest_framework import viewsets, serializers, status
 from strugglebusapi.models import Tasks
+from rest_framework.response import Response
 # Create your views here.
 
 
 class TasksSerializer(serializers.ModelSerializer):
-    """JSON serializer for games
-    Arguments:
-        serializer type
-    """
     class Meta:
         model = Tasks
         fields = ('id', 'name', 'completed')
         depth = 1
 
 
-class TasksViewSet(viewsets.ModelViewSet):
-    queryset = Tasks.objects.all()
-    serializer_class = TasksSerializer
+class TasksViewSet(viewsets.ViewSet):
+    # queryset = Tasks.objects.all()
+    # serializer_class = TasksSerializer
+
+    def list(self, request):
+        tasks = Tasks.objects.all()
+        tasks = TasksSerializer(tasks, many=True, context={'request': request})
+
+        return Response(tasks.data)
+
+    def update(self, request, pk=None):
+        task = Tasks.objects.get(pk=pk)
+        task.name = request.data['name']
+        try:
+            task.save()
+            task = TasksSerializer(task, many=False, context={
+                                   'request': request})
+            return Response(task.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
